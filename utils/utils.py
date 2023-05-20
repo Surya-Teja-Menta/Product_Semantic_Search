d20c21dce7d41635b2392300c95141b217f387a7
Surya Teja Menta <79039074+Surya-Teja-Menta@users.noreply.github.com>
2023-05-21 02:11:14 +0530

    Added Successfully

--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,5 @@
-import pandas as pd
-from sentence_transformers import SentenceTransformer
+import pandas as pd,numpy as np
+from sentence_transformers import SentenceTransformer,util
 import pickle
 from sklearn.metrics.pairwise import cosine_similarity
 
@@ -14,13 +14,13 @@ def get_similar_items(text, n=10):
     with open('utils/stopwords.pkl', 'rb') as file:
         stopwords = pickle.load(file)
         file.close()
-    df = pd.read_pickle('data/final_myntra3.pkl')
+    df = pd.DataFrame(np.load('data/df1.npy',allow_pickle = True),columns = ['title','link','price','embedding','score'])
     text = ' '.join([x for x in text.split() if x not in stopwords])
     model = get_model()
     embedds = model.encode(text)
-    df["score"] = df.embedding.apply(lambda x: cosine_similarity(x, embedds))
+    df["score"] = df.embedding.apply(lambda x: util.cos_sim(x, embedds))
     top_n =df.sort_values(["score"], ascending=False).head(n)
-    return top_n[['title','link-href','price']]
+    return top_n[['title','link','price']]
 
     
 
