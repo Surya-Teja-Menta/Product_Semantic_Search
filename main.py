03e9a4d7247a3e234d443475b87cb369a21be499
Surya Teja Menta <79039074+Surya-Teja-Menta@users.noreply.github.com>
2023-05-21 17:10:08 +0530

    Added Successfully

--- main.py
+++ main.py
@@ -2,18 +2,10 @@ import sys,json
 import pandas as pd
 from utils.utils import *
 
-print(sys.argv[1])
-# Get the top 5 similar items
-similar_items = get_similar_items(sys.argv[1], 10)
+if __name__ == '__main__':
 
-data = similar_items.reset_index(drop=True).values.tolist()
-result = []
-for item in data:
-    title, link, price = item
-    result.append({"titles": title, "link": link, "price": price})
-
-# Convert dictionary to JSON
-json_data = json.dumps(result)
-
-# Print the JSON data
-print(json_data)
+    # Get the top 10 similar items
+    similar_items = get_similar_items(sys.argv[1], 10)
+    
+    # Print the JSON data
+    print(similar_items)
--- utils/utils.py
+++ utils/utils.py
@@ -1,14 +1,24 @@
-import pandas as pd,numpy as np
+import pandas as pd,numpy as np,json
 from sentence_transformers import SentenceTransformer,util
 import pickle
 from sklearn.metrics.pairwise import cosine_similarity
 
 def get_model():
+    """
+    This function returns a SentenceTransformer model.
+    """
     return SentenceTransformer('all-MiniLM-L6-v2')
 
 def get_similar_items(text, n=10):
     """
-    Get top 10 similar items for the given item
+    This function gets the top 10 similar items for the given item.
+
+    Args:
+        text (str): The item to get similar items for.
+        n (int, optional): The number of similar items to get. Defaults to 10.
+
+    Returns:
+        str: A JSON string containing the top N most similar items.
     """
     text = text.lower()
     with open('utils/stopwords.pkl', 'rb') as file:
@@ -20,7 +30,18 @@ def get_similar_items(text, n=10):
     embedds = model.encode(text)
     df["score"] = df.embedding.apply(lambda x: util.cos_sim(x, embedds))
     top_n =df.sort_values(["score"], ascending=False).head(n)
-    return top_n[['title','link','price']]
+
+    similar_items =top_n[['title','link','price']]
+    data = similar_items.reset_index(drop=True).values.tolist()
+
+    # Create a list of dictionaries.
+    result = []
+    for item in data:
+        title, link, price = item
+        result.append({"titles": title, "link": link, "price": price})
+
+    # Convert the list of dictionaries to JSON.
+    return json.dumps(result)
 
     
 
